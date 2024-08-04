07d731ea6df6b76d9d06b89cfa72c05b5688d26c
Webdeweloper77 <98384659+Webdeweloper77@users.noreply.github.com>
2024-08-04 21:43:58 +0400

    Your commit message

--- cosmosConfig.js
+++ cosmosConfig.js
@@ -3,10 +3,32 @@ import { CosmosClient } from '@azure/cosmos';
 const endpoint = process.env.COSMOS_DB_ENDPOINT;
 const key = process.env.COSMOS_DB_KEY;
 const databaseId = process.env.COSMOS_DB_NAME;
-const containerId = process.env.COSMOS_DB_CONTAINER; // Используйте ваш идентификатор контейнера
+const containerId = process.env.COSMOS_DB_CONTAINER;
 
-const client = new CosmosClient({ endpoint, key });
-const database = client.database(databaseId);
-const container = database.container(containerId);
+console.log("COSMOS_DB_ENDPOINT:", endpoint);
+console.log("COSMOS_DB_KEY:", key ? "****" : "undefined");
+console.log("COSMOS_DB_NAME:", databaseId);
+console.log("COSMOS_DB_CONTAINER:", containerId);
+
+let container;
+
+const connectToCosmosDB = async () => {
+    try {
+        const client = new CosmosClient({ endpoint, key });
+        const database = client.database(databaseId);
+        container = database.container(containerId);
+
+        const { resource: databaseResource } = await database.read();
+        console.log(`Connected to database: ${databaseResource.id}`);
+
+        const { resource: containerResource } = await container.read();
+        console.log(`Connected to container: ${containerResource.id}`);
+    } catch (error) {
+        console.error("Error connecting to Cosmos DB:", error);
+        throw error;
+    }
+};
+
+connectToCosmosDB();
 
 export { container };
